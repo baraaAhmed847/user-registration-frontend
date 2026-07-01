efe784efb008e5255672ce4e0c637371b0b5ce2b
baraaAhmed847 <baraa.a.dev@gmail.com>
2026-07-01 19:29:37 +0300

    Fix CORS

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'https://user-registration-frontend-production.up.railway.app/api'; // Replace with your backend API URL
+const API_URL = process.env.REACT_APP_API_URL + '/api' || 'http://localhost:5000/api'; // Replace with your backend API URL
 
 const api = axios.create({
     baseURL: API_URL,
